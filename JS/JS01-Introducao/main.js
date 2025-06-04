2fbade58ace4b73c065e728a6d32a8739b350ed4
MTHS011 <mthsaraujo98@gmail.com>
2025-06-03 21:39:55 -0300

    inicio JS

---
+++ JS/JS01-Introducao/main.js
@@ -0,0 +1,30 @@
+var idade 
+
+//JavaScript
+console.log("Hello World") // apresenta uma mensagem
+
+// Variavies
+var numero1 = 190 // inicializar uma variavel
+console.log(numero1)
+
+idade = 27 // iniciar a variavel em outra parte do codigo
+console.log(idade)
+
+var num1 = 100
+var num2 = 200
+var resultado = num1 + num2 
+console.log(resultado)
+
+
+// Textos - Strings
+var nome = 'Matheus Araujo'
+var nome2 = "Matheus Vinicius"
+var nome3 = "987654321"
+console.log(nome, nome2, nome3)
+
+
+VAR // flexivel e com menos regras (posso alterar o valor inicial de uma var e recriar a mesma em outras partes do codigo)
+
+LEF // voce nao pode recriar a mesma em outras partes do codigo com o mesmo nome, mas pode chamar e alterar o valor inicial
+
+CONST // voce pode recriar a mesma em outras partes do codigo e nao pode alterar o valor inicial dela garantindo a confianca do dado que esta sendo utilizadono decorrer do seu codigo
