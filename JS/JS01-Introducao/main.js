ccff8f89bd94bca447a90486f2a15cc6345e3bb7
MTHS011 <mthsaraujo98@gmail.com>
2025-06-04 21:34:51 -0300

    aula JS

--- JS/JS01-Introducao/main.js
+++ JS/JS01-Introducao/main.js
@@ -22,9 +22,51 @@ var nome2 = "Matheus Vinicius"
 var nome3 = "987654321"
 console.log(nome, nome2, nome3)
 
+console.log("Olá", "papai") // Concatenar 1 
 
-VAR // flexivel e com menos regras (posso alterar o valor inicial de uma var e recriar a mesma em outras partes do codigo)
+console.log("Aluno" + nome + "e aluno" + nome2 + "tem um total de" + nome3 + "dinheiros") // Concatenar 2
 
-LEF // voce nao pode recriar a mesma em outras partes do codigo com o mesmo nome, mas pode chamar e alterar o valor inicial
+// Template String
+console.log(`Aluno ${nome} e aluno ${nome2} tem um total de ${nome3} dinheiros`) // Concatenar 3 
 
-CONST // voce pode recriar a mesma em outras partes do codigo e nao pode alterar o valor inicial dela garantindo a confianca do dado que esta sendo utilizadono decorrer do seu codigo
+
+//VAR  flexivel e com menos regras (posso alterar o valor inicial de uma var e recriar a mesma em outras partes do codigo)
+
+//LEF  voce nao pode recriar a mesma em outras partes do codigo com o mesmo nome, mas pode chamar e alterar o valor inicial
+
+//CONST  voce pode recriar a mesma em outras partes do codigo e nao pode alterar o valor inicial dela garantindo a confianca do dado que esta sendo utilizadono decorrer do seu codigo
+
+// Operadores Aritmericos
+var valor1 = 5
+var valor2 = 5
+console.log(valor1 + valor2)
+console.log(valor1 - valor2)
+console.log(valor1 * valor2)
+console.log(valor1 / valor2)
+console.log(valor1 ** valor2)
+
+// Operadores relacionais
+console.log(5 == 5)
+console.log(5 > 5)
+console.log(22 < 8)
+console.log(22 >= 22)
+console.log(22 != 23)
+console.log("Ana" == "Ana")
+console.log(true == false)
+console.log(true === "true") // boolean e o outro e txt > String
+console.log(true != "false") // true
+
+let temCamisa = false
+let temCracha = true
+console.log(temCamisa)
+console.log(temCracha)
+console.log(temCamisa == true && temCracha == true)
+console.log(temCamisa == true || temCrash == true)
+
+
+
+
+
+
+
+ 
