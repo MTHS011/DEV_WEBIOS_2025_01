2d9ab1d480bf242aeb3363e9a130c53694c73026
MTHS011 <mthsaraujo98@gmail.com>
2025-06-10 21:32:30 -0300

    aula JS02

---
+++ JS/JS02-Strings/main.js
@@ -0,0 +1,32 @@
+var texto1 = "Olá IOS"
+
+var resultado1 = texto1.charAt(4) // 
+
+
+
+
+
+var lorem = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+
+console.log(lorem.length)
+
+var texto2 = "OLÁ TEC DE TECNOLOGIA"
+var texto3 = "olá, meu nome é ia, seu amigo"
+
+// Formato1
+var resultado3 = texto2.toLowerCase()
+console.log(resultado3)
+
+// Formato 2 
+console.log(texto3.toUpperCase())
+
+
+var texto4 = "Mozzila"
+console.log(texto4.substring(1, 3))
+
+console.log(texto4.substring(1))
+
+var texto5 = "Bem vindo a Microsoft"
+let novaMensagem = texto5.replace("Microsoft", "IOS")
+console.log(novaMensagem)
+
