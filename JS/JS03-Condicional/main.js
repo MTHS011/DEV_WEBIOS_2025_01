00fbf9d0de77fa8af277d0cf6a9e5f4b1e4c63a4
MTHS011 <mthsaraujo98@gmail.com>
2025-06-12 20:10:42 -0300

    fim aula js03

---
+++ JS/JS03-Condicional/main.js
@@ -0,0 +1,12 @@
+var nota = 6.9
+// se a nota for igual ou maior a 7, o aluno passou
+//se a nota for igual ou menos a 6.9, o aluno reprovou
+if(nota >= 7){
+    console.log("Passou na materia")
+    
+}else{
+    console.log("Vai ficar de recuperaçao")
+}
+
+nota = 10
+nota >= 7 ? console.log("Passou na materia") : console.log("Vai ficar de recuperaçao")
---
+++ JS/JS03-Condicional/mainExercicio.js
@@ -0,0 +1,9 @@
+var n1 = 10
+var n2 = 5 
+
+if(n1 >= n2){
+    console.log("Numero 1 é maior que numero 2")
+
+} else{
+    console.log("Numero 1 é menor que numero 2")
+}
