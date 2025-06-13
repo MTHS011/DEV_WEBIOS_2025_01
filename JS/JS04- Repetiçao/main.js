ca8214a4989e84b55692cac27b37d06709f14b91
MTHS011 <mthsaraujo98@gmail.com>
2025-06-12 21:10:47 -0300

    Exercicio finalizado

---
+++ JS/JS04- Repetiçao/main.js
@@ -0,0 +1,14 @@
+let contador = 0 
+// Enquanto a variavel contador for menor que 10 while vai apresentar o valor da variavel no console 
+while(contador < 10){
+    console.log(contador)
+    //contador = contador + 1
+    //contador +=1
+    contador++
+
+}
+
+//variavel inicial; condiçao; incremento
+for(contador = 0; contador <= 10; contador++){
+    console.log(contador)
+}
